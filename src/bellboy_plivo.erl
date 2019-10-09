1aa5391058e096a6b914806e8cf38d6d52e611b8
Viacheslav Katsuba <v.katsuba.dev@gmail.com>
2019-10-09 13:10:01 +0300

    Added GET Ploivo all messages

--- src/bellboy_plivo.erl
+++ src/bellboy_plivo.erl
@@ -23,9 +23,17 @@
 %% @end
 %% -------------------------------------------------------------------
 
-message(#{type := send_message} = Data) -> send_message(Data);
-message(#{type := get_message} = Data)  -> get_message(Data);
-message(_) -> ?BAD_ARG.
+message(#{type := send_message} = Data) ->
+  send_message(Data);
+
+message(#{type := get_message} = Data) ->
+  get_message(Data);
+
+message(#{type := get_messages} = Data) ->
+  get_messages(Data);
+
+message(_) ->
+  ?BAD_ARG.
 
 %%% ==================================================================
 %%% Internal/Private functions
@@ -42,10 +50,14 @@ message(_) -> ?BAD_ARG.
 send_message(#{auth_id := AID, auth_token := AT, payload := P}) when is_list(AID), is_list(AT), is_map(P) ->
   RD = #{m => post, u => ?PLIVO_URL_MSG(AID), h => #{"Authorization" => ?BASIC_AUTH(AID, AT)}, b => jiffy:encode(P), ct => "application/json"},
   case bellboy_utils:httpc_request(RD) of
-    {ok, Resp} -> {ok, #{code => bellboy_utils:get_code(Resp), body => gen_resp_body(bellboy_utils:get_body(Resp)), response => Resp}};
-    Error -> Error
+    {ok, Resp} ->
+      {ok, #{code => bellboy_utils:get_code(Resp), body => gen_resp_body(bellboy_utils:get_body(Resp)), response => Resp}};
+    Error ->
+      Error
   end;
-send_message(_) -> ?BAD_ARG.
+
+send_message(_) ->
+  ?BAD_ARG.
 
 %% -------------------------------------------------------------------
 %% @private
@@ -58,11 +70,35 @@ send_message(_) -> ?BAD_ARG.
 get_message(#{auth_id := AID, auth_token := AT, message_uuid := MUUID}) when is_list(AID), is_list(AT), is_list(MUUID) ->
   RD = #{m => get, u => ?PLIVO_URL_MSG(AID) ++ MUUID, h => #{"Authorization" => ?BASIC_AUTH(AID, AT)}},
   case bellboy_utils:httpc_request(RD) of
-    {ok, Resp} -> {ok, #{code => bellboy_utils:get_code(Resp), body => gen_resp_body(bellboy_utils:get_body(Resp)), response => Resp}};
-    Error -> Error
+    {ok, Resp} ->
+      {ok, #{code => bellboy_utils:get_code(Resp), body => gen_resp_body(bellboy_utils:get_body(Resp)), response => Resp}};
+    Error ->
+      Error
   end;
 
-get_message(_) -> ?BAD_ARG.
+get_message(_) ->
+  ?BAD_ARG.
+
+%% -------------------------------------------------------------------
+%% @private
+%% @doc
+%% Get SMS messages
+%% @end
+%% -------------------------------------------------------------------
+-spec get_messages(Params :: maps:map()) -> {ok, Result :: maps:map()} | {error, Reason :: tuple() | bad_arg}.
+
+get_messages(#{auth_id := AID, auth_token := AT}) when is_list(AID), is_list(AT) ->
+  RD = #{m => get, u => ?PLIVO_URL_MSG(AID), h => #{"Authorization" => ?BASIC_AUTH(AID, AT)}},
+  case bellboy_utils:httpc_request(RD) of
+    {ok, Resp} ->
+      {ok, #{code => bellboy_utils:get_code(Resp), body => gen_resp_body(bellboy_utils:get_body(Resp)), response => Resp}};
+    Error ->
+      Error
+  end;
+
+get_messages(_) ->
+  ?BAD_ARG.
+
 %% -------------------------------------------------------------------
 %% @private
 %% @doc
