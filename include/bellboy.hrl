22232a24c885d8bc6560dc29cd8a90f0f69d5cb3
vkatsuba <viacheslav.katsuba@travelping.com>
2021-02-20 11:51:28 +0200

    Remove uuid non use dependency

--- include/bellboy.hrl
+++ include/bellboy.hrl
@@ -9,7 +9,6 @@
 -define(CIPHERS, [[{ciphers, [{rsa, aes_128_cbc, sha}]}]]).
 -define(SSLC, {ssl, ?CIPHERS}).
 -define(BAD_ARG, {error, bad_arg}).
--define(GEN_UUID_V4, list_to_binary(uuid:uuid_to_string(uuid:get_v4()))).
 -define(PLIVO_URL_MSG(AuthID), "https://api.plivo.com/v1/Account/" ++ AuthID ++ "/Message/").
 -define(TWILIO_URL_MSG(AuthID), "https://api.twilio.com/2010-04-01/Accounts/" ++ AuthID ++ "/Messages.json").
 -define(TWILIO_URL_SPEC_MSG(AuthID, Sid), "https://api.twilio.com/2010-04-01/Accounts/" ++ AuthID ++ "/Messages/" ++ Sid ++ ".json").
