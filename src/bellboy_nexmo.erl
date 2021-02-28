9bede371f38e59f611df1839afadfc0b6e9e60ad
vkatsuba <viacheslav.katsuba@travelping.com>
2021-02-28 17:29:41 +0200

    Refactoring of bellboy_nexmo.erl

--- src/bellboy_nexmo.erl
+++ src/bellboy_nexmo.erl
@@ -50,19 +50,15 @@
 %% -------------------------------------------------------------------
 
 message(#{type := send_sms} = Data) ->
-  send_sms(maps:without([type], Data));
-
+    send_sms(maps:without([type], Data));
 message(#{type := send_pin} = Data) ->
-  send_pin(maps:without([type], Data));
-
+    send_pin(maps:without([type], Data));
 message(#{type := cancel_pin} = Data) ->
-  cancel_pin(maps:without([type], Data));
-
+    cancel_pin(maps:without([type], Data));
 message(#{type := check_pin} = Data) ->
-  check_pin(maps:without([type], Data));
-
+    check_pin(maps:without([type], Data));
 message(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
 
 %%% ==================================================================
 %%% Internal/Private functions
@@ -77,21 +73,22 @@ message(_) ->
 -spec send_sms(Params :: maps:map()) -> {ok, Result :: maps:map()} | {error, Reason :: tuple() | bad_arg}.
 
 send_sms(#{from := F, text := TXT, to := T, api_key := AK, api_secret := AC} = B) ->
-  case bellboy_utils:is_valid([is_binary(F), is_binary(TXT), is_binary(T), is_binary(AK), is_binary(AC)]) of
-    true ->
-      RD = #{m => post, u => ?NEXMO_URL_MSG, ct => "application/json", b => jsx:encode(B)},
-      case bellboy_utils:httpc_request(RD) of
-        {ok, Resp} ->
-          {ok, #{code => bellboy_utils:get_code(Resp), body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)), response => Resp}};
-        Error ->
-          Error
-      end;
-    false ->
-      ?BAD_ARG
-  end;
-
+    case bellboy_utils:is_valid([is_binary(F), is_binary(TXT), is_binary(T), is_binary(AK), is_binary(AC)]) of
+        true ->
+            RD = #{m => post, u => ?NEXMO_URL_MSG, ct => "application/json", b => jsx:encode(B)},
+            case bellboy_utils:httpc_request(RD) of
+                {ok, Resp} ->
+                    {ok, #{code => bellboy_utils:get_code(Resp),
+                           body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)),
+                           response => Resp}};
+                Error ->
+                    Error
+            end;
+        false ->
+            ?BAD_ARG
+    end;
 send_sms(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
 
 %% -------------------------------------------------------------------
 %% @private
@@ -102,22 +99,27 @@ send_sms(_) ->
 -spec send_pin(Params :: maps:map()) -> {ok, Result :: maps:map()} | {error, Reason :: tuple() | bad_arg}.
 
 send_pin(#{api_key := AK, api_secret := AC, number := N, brand := B, code_length := CL}) ->
-  case bellboy_utils:is_valid([is_list(AK), is_list(AC), is_list(N), is_list(B), is_list(CL)]) of
-    true ->
-      URI = "?api_key=" ++ http_uri:encode(AK) ++ "&api_secret=" ++ http_uri:encode(AC) ++ "&number=" ++ http_uri:encode(N) ++ "&brand=" ++ http_uri:encode(B) ++ "&code_length=" ++ http_uri:encode(CL),
-      RD = #{m => get, u => ?NEXMO_URL_VERIFY ++ URI},
-      case bellboy_utils:httpc_request(RD) of
-        {ok, Resp} ->
-          {ok, #{code => bellboy_utils:get_code(Resp), body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)), response => Resp}};
-        Error ->
-          Error
-      end;
-    false ->
-      ?BAD_ARG
-  end;
-
+    case bellboy_utils:is_valid([is_list(AK), is_list(AC), is_list(N), is_list(B), is_list(CL)]) of
+        true ->
+            URI = "?api_key=" ++ http_uri:encode(AK) ++
+                  "&api_secret=" ++ http_uri:encode(AC) ++
+                  "&number=" ++ http_uri:encode(N) ++
+                  "&brand=" ++ http_uri:encode(B) ++
+                  "&code_length=" ++ http_uri:encode(CL),
+            RD = #{m => get, u => ?NEXMO_URL_VERIFY ++ URI},
+            case bellboy_utils:httpc_request(RD) of
+                {ok, Resp} ->
+                    {ok, #{code => bellboy_utils:get_code(Resp),
+                           body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)),
+                           response => Resp}};
+                Error ->
+                    Error
+            end;
+        false ->
+            ?BAD_ARG
+    end;
 send_pin(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
 
 %% -------------------------------------------------------------------
 %% @private
@@ -128,17 +130,21 @@ send_pin(_) ->
 -spec cancel_pin(Params :: maps:map()) -> {ok, Result :: maps:map()} | {error, Reason :: tuple() | bad_arg}.
 
 cancel_pin(#{api_key := AK, api_secret := AC, request_id := RID}) when is_list(AK), is_list(AC), is_list(RID) ->
-  URI = "?api_key=" ++ http_uri:encode(AK) ++ "&api_secret=" ++ http_uri:encode(AC) ++ "&cmd=cancel" ++ "&request_id=" ++ http_uri:encode(RID),
-  RD = #{m => get, u => ?NEXMO_URL_CONTROL ++ URI},
-  case bellboy_utils:httpc_request(RD) of
-    {ok, Resp} ->
-      {ok, #{code => bellboy_utils:get_code(Resp), body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)), response => Resp}};
-    Error ->
-      Error
-  end;
-
+    URI = "?api_key=" ++ http_uri:encode(AK) ++
+          "&api_secret=" ++ http_uri:encode(AC) ++
+          "&cmd=cancel" ++ "&request_id=" ++
+          http_uri:encode(RID),
+    RD = #{m => get, u => ?NEXMO_URL_CONTROL ++ URI},
+    case bellboy_utils:httpc_request(RD) of
+        {ok, Resp} ->
+            {ok, #{code => bellboy_utils:get_code(Resp),
+                   body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)),
+                   response => Resp}};
+        Error ->
+            Error
+    end;
 cancel_pin(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
 
 %% -------------------------------------------------------------------
 %% @private
@@ -149,14 +155,18 @@ cancel_pin(_) ->
 -spec check_pin(Params :: maps:map()) -> {ok, Result :: maps:map()} | {error, Reason :: tuple() | bad_arg}.
 
 check_pin(#{api_key := AK, api_secret := AC, request_id := RID, code := C}) when is_list(AK), is_list(AC), is_list(RID), is_list(C) ->
-  URI = "?api_key=" ++ http_uri:encode(AK) ++ "&api_secret=" ++ http_uri:encode(AC) ++ "&request_id=" ++ http_uri:encode(RID) ++ "&code=" ++ http_uri:encode(C),
-  RD = #{m => get, u => ?NEXMO_URL_CHECK ++ URI},
-  case bellboy_utils:httpc_request(RD) of
-    {ok, Resp} ->
-      {ok, #{code => bellboy_utils:get_code(Resp), body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)), response => Resp}};
-    Error ->
-      Error
-  end;
-
+    URI = "?api_key=" ++ http_uri:encode(AK) ++
+          "&api_secret=" ++ http_uri:encode(AC) ++
+          "&request_id=" ++ http_uri:encode(RID) ++
+          "&code=" ++ http_uri:encode(C),
+    RD = #{m => get, u => ?NEXMO_URL_CHECK ++ URI},
+    case bellboy_utils:httpc_request(RD) of
+        {ok, Resp} ->
+            {ok, #{code => bellboy_utils:get_code(Resp),
+                   body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)),
+                   response => Resp}};
+        Error ->
+            Error
+    end;
 check_pin(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
