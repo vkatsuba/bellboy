ce499e81a7f47591eea899b2c01250a8d0c657f0
vkatsuba <viacheslav.katsuba@travelping.com>
2021-03-07 11:10:03 +0200

    Refactoring bellboy_plivo.erl

--- src/bellboy_plivo.erl
+++ src/bellboy_plivo.erl
@@ -47,16 +47,16 @@
 %% -------------------------------------------------------------------
 
 message(#{type := send_message} = Data) ->
-  send_message(Data);
+    send_message(Data);
 
 message(#{type := get_message} = Data) ->
-  get_message(Data);
+    get_message(Data);
 
 message(#{type := get_messages} = Data) ->
-  get_messages(Data);
+    get_messages(Data);
 
 message(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
 
 %%% ==================================================================
 %%% Internal/Private functions
@@ -71,17 +71,22 @@ message(_) ->
 -spec send_message(Params :: maps:map()) -> {ok, Result :: maps:map()} | {error, Reason :: tuple() | bad_arg}.
 
 send_message(#{auth_id := AID, auth_token := AT} = Data) when is_list(AID), is_list(AT) ->
-  P = maps:without([type, auth_id, payload], Data),
-  RD = #{m => post, u => ?PLIVO_URL_MSG(AID), h => #{"Authorization" => bellboy_utils:basic_auth(AID, AT)}, b => jsx:encode(P), ct => "application/json"},
-  case bellboy_utils:httpc_request(RD) of
-    {ok, Resp} ->
-      {ok, #{code => bellboy_utils:get_code(Resp), body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)), response => Resp}};
-    Error ->
-      Error
-  end;
+    P = maps:without([type, auth_id, payload], Data),
+    RD = #{m => post,
+           u => ?PLIVO_URL_MSG(AID),
+           h => #{"Authorization" => bellboy_utils:basic_auth(AID, AT)},
+           b => jsx:encode(P), ct => "application/json"},
+    case bellboy_utils:httpc_request(RD) of
+        {ok, Resp} ->
+            {ok, #{code => bellboy_utils:get_code(Resp),
+                   body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)),
+                   response => Resp}};
+        Error ->
+            Error
+    end;
 
 send_message(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
 
 %% -------------------------------------------------------------------
 %% @private
@@ -92,16 +97,20 @@ send_message(_) ->
 -spec get_message(Params :: maps:map()) -> {ok, Result :: maps:map()} | {error, Reason :: tuple() | bad_arg}.
 
 get_message(#{auth_id := AID, auth_token := AT, message_uuid := MUUID}) when is_list(AID), is_list(AT), is_list(MUUID) ->
-  RD = #{m => get, u => ?PLIVO_URL_MSG(AID) ++ MUUID, h => #{"Authorization" => bellboy_utils:basic_auth(AID, AT)}},
-  case bellboy_utils:httpc_request(RD) of
-    {ok, Resp} ->
-      {ok, #{code => bellboy_utils:get_code(Resp), body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)), response => Resp}};
-    Error ->
-      Error
-  end;
+    RD = #{m => get,
+           u => ?PLIVO_URL_MSG(AID) ++ MUUID,
+           h => #{"Authorization" => bellboy_utils:basic_auth(AID, AT)}},
+    case bellboy_utils:httpc_request(RD) of
+        {ok, Resp} ->
+            {ok, #{code => bellboy_utils:get_code(Resp),
+                   body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)),
+                   response => Resp}};
+        Error ->
+            Error
+    end;
 
 get_message(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
 
 %% -------------------------------------------------------------------
 %% @private
@@ -112,13 +121,17 @@ get_message(_) ->
 -spec get_messages(Params :: maps:map()) -> {ok, Result :: maps:map()} | {error, Reason :: tuple() | bad_arg}.
 
 get_messages(#{auth_id := AID, auth_token := AT}) when is_list(AID), is_list(AT) ->
-  RD = #{m => get, u => ?PLIVO_URL_MSG(AID), h => #{"Authorization" => bellboy_utils:basic_auth(AID, AT)}},
-  case bellboy_utils:httpc_request(RD) of
-    {ok, Resp} ->
-      {ok, #{code => bellboy_utils:get_code(Resp), body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)), response => Resp}};
-    Error ->
-      Error
-  end;
+    RD = #{m => get,
+           u => ?PLIVO_URL_MSG(AID),
+           h => #{"Authorization" => bellboy_utils:basic_auth(AID, AT)}},
+    case bellboy_utils:httpc_request(RD) of
+        {ok, Resp} ->
+            {ok, #{code => bellboy_utils:get_code(Resp),
+            body => bellboy_utils:gen_body(bellboy_utils:get_body(Resp)),
+            response => Resp}};
+        Error ->
+            Error
+    end;
 
 get_messages(_) ->
-  ?BAD_ARG.
+    ?BAD_ARG.
