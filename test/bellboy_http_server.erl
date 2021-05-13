2f1e01a457cf0f5c64181b6a0b65b5de495e6e02
Manuel Rubio <manuel@altenwald.com>
2021-05-13 14:29:37 +0200

    add first test

---
+++ test/bellboy_http_server.erl
@@ -0,0 +1,79 @@
+-module(bellboy_http_server).
+-author('Steve Vinoski <vinoski@ieee.org>').
+-export([start/1, start/2, stop/1]).
+
+%% start/1 takes a handler function and starts the web server on port 8000.
+
+%% start/2 takes a handler function and a port number. The handler function
+%% takes two arguments: a TCP socket and request data. The request data is
+%% a property list indicating the invoked HTTP method, the target URI, the
+%% HTTP version of the request, and a list of HTTP headers sent with the
+%% request. For requests that provide data such as PUT and POST, the
+%% handler function is expected to read the body off the socket. The
+%% handler function returns a 3-tuple indicating the response HTTP status
+%% code, the response HTTP headers in the form of {header,value} pairs, and
+%% any response body. The start/2 function waits to receive a stop atom,
+%% indicating it should close the listen socket and exit.
+start(Handler) ->
+    {ok, spawn(fun() -> start(Handler, 8000) end)}.
+
+start(Handler, Port) ->
+    {ok, LS} = gen_tcp:listen(Port, [{reuseaddr,true},binary,{backlog,1024}]),
+    spawn(fun() -> accept(LS, Handler) end),
+    receive
+        {stop, StopPID} ->
+            gen_tcp:close(LS),
+            StopPID ! ok
+    end.
+
+stop(PID) when is_pid(PID) ->
+    PID ! {stop, self()},
+    receive
+        ok -> ok
+    after 100 -> error
+    end.
+
+%% The accept/2 function accepts a connection, spawns a new acceptor, and
+%% then handles its incoming request.
+accept(LS, Handler) ->
+    case gen_tcp:accept(LS) of
+        {ok, S} ->
+            ok = inet:setopts(S, [{packet,http_bin}]),
+            spawn(fun() -> accept(LS, Handler) end),
+            serve(S, Handler, [{headers, []}]);
+        {error, closed} ->
+            ok
+    end.
+
+%% The serve/3 function reads the request headers, assembles the request
+%% data property list, calls the handler/2 function to handle the request,
+%% and assembles and returns the response.
+serve(S, Handler, Req) ->
+    ok = inet:setopts(S, [{active, once}]),
+    HttpMsg = receive
+                  {http, S, Msg} -> Msg;
+                  _ -> gen_tcp:close(S)
+              end,
+    case HttpMsg of
+        {http_request, M, {abs_path, Uri}, Vsn} ->
+            NReq = [{method,M},{uri,Uri},{version,Vsn}|Req],
+            serve(S, Handler, NReq);
+        {http_header, _, Hdr, _, Val} ->
+            {headers, Hdrs} = lists:keyfind(headers, 1, Req),
+            serve(S, Handler, lists:keystore(headers, 1, Req,
+                                             {headers, [{Hdr,Val}|Hdrs]}));
+        http_eoh ->
+            ok = inet:setopts(S, [{packet, raw}]),
+            {RawStatus, Hdrs, Resp} = try Handler(S, Req) catch _:_ -> {500, [], <<>>} end,
+            Status = if
+                RawStatus =:= 200 -> "200 OK";
+                is_list(RawStatus) -> RawStatus
+            end,
+            ok = gen_tcp:send(S, ["HTTP/1.1 ", Status, "\r\n",
+                                  [[H, ": ", V, "\r\n"] || {H,V} <- Hdrs],
+                                  "\r\n", Resp]),
+            gen_tcp:close(S);
+        {http_error, Error} ->
+            exit(Error);
+        ok -> ok
+    end.
---
+++ test/bellboy_nexmo_test.erl
@@ -0,0 +1,62 @@
+-module(bellboy_nexmo_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+http_handler(PID, _Socket, Request) ->
+    PID ! Request,
+    Content = <<
+        "{\n    \"message-count\": \"1\",\n    "
+        "\"messages\": [{\n        \"to\": \"34666666666\",\n        "
+        "\"message-id\": \"14000001A4ECACC7\",\n        "
+        "\"status\": \"0\",\n        \"remaining-balance\": \"1.92050000\","
+        "\n        \"message-price\": \"0.07950000\","
+        "\n        \"network\": \"20416\"\n    }]\n}"
+    >>,
+    {200, [
+        {"cache-control","max-age=1"},
+        {"connection","keep-alive"},
+        {"date","Thu, 13 May 2021 10:32:53 GMT"},
+        {"server","nginx"},
+        {"content-length","258"},
+        {"content-type","application/json"},
+        {"x-frame-options","deny"},
+        {"x-xss-protection","1; mode=block;"},
+        {"strict-transport-security", "max-age=31536000; includeSubdomains"},
+        {"content-disposition","attachment; filename=\"api.txt\""}, 
+        {"x-nexmo-trace-id","0feb8e00eb8eb445ae7be4798fdcf9f9"}
+    ], Content}.
+
+nexmo_sms_test() ->
+    Parent = self(),
+    {ok, PID} = bellboy_http_server:start(fun(S, Req) -> http_handler(Parent, S, Req) end),
+    application:set_env(bellboy, nexmo_url_msg, "http://localhost:8000"),
+    {ok, #{body := RawBody, code := 200, response := {{"HTTP/1.1", 200, "OK"}, [_|_], RawBody}}} =
+        bellboy:nexmo(#{
+            type => send_sms,
+            from => <<"Vonage APIs">>,
+            to => <<"34666666666">>,
+            api_key => <<"01234567">>,
+            api_secret => <<"0123456789abcdef">>,
+            text => <<"Your activation code is XXXX">>
+        }),
+    Body = jsone:decode(iolist_to_binary(RawBody)),
+    ?assertEqual(
+        #{
+            <<"message-count">> => <<"1">>,
+            <<"messages">> => [#{
+                <<"message-id">> => <<"14000001A4ECACC7">>,
+                <<"message-price">> => <<"0.07950000">>,
+                <<"network">> => <<"20416">>,
+                <<"remaining-balance">> => <<"1.92050000">>,
+                <<"status">> => <<"0">>,
+                <<"to">> => <<"34666666666">>}
+            ]
+        },
+        Body
+    ),
+    receive
+        [{method, 'POST'}|_] -> ok
+    after 1000 -> throw(timeout)
+    end,
+    bellboy_http_server:stop(PID),
+    ok.
